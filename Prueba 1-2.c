e8178f959a87a42d8473ca75dd89619f23fc0725
Setroya <setroy4s@gmail.com>
2020-08-04 17:51:58 -0400

    tercero-2

--- Prueba 1-2.c
+++ Prueba 1-2.c
@@ -2,6 +2,10 @@ void busqueda_binaria(int n,struct *L){
     struct lista *lista=L;
     int lugar;
     int nohay=-1;
+    while(lista->siguiente!=NULL){
+           if(lista->actual->numero==n){
+            lugar=lista->actual->lugar-1
+           }
  
-       
+    }
    }
